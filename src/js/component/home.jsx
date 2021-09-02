ecd9ac9b7e4e0d3d91d05b8de324e45a8da2330c
Joaquinabarca95 <joaquinabarca@live.com>
2021-09-02 20:41:28 +0000

    finished proyect

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useRef, useState } from "react";
 
 //create your first component
 const Home = () => {
@@ -23,34 +23,95 @@ const Home = () => {
 		}
 	]);
 
-	const Songs = musica.map((value, index) => {
+	let audioRef = useRef();
+
+	const [current, setCurrent] = useState(0);
+	const [isPlaying, setIsPlaying] = useState(false);
+
+	const songURL = musica.map(song => {
+		return "https://assets.breatheco.de/apis/sound/" + song.url;
+	});
+
+	const songs = musica.map((song, index) => {
 		return (
-			<li key={value.id}>
-				{index} {value.name}
+			<li
+				key={song.id}
+				className={
+					isPlaying == true && current === index ? "active" : ""
+				}>
+				{index} {song.name}
 			</li>
 		);
 	});
 
-	const songURL = musica.map(value => {
-		return "https://assets.breatheco.de/apis/sound/" + value.url;
-	});
+	const togglePlayPause = () => {
+		setIsPlaying(!isPlaying);
+		audioRef.current.src = songURL[current];
+		isPlaying ? audioRef.current.pause() : audioRef.current.play();
+	};
+
+	const nextSong = () => {
+		if (current === songs.length - 1) {
+			setCurrent(0);
+			audioRef.current.src = songURL[0];
+			setIsPlaying(true);
+			audioRef.current.play();
+		} else {
+			audioRef.current.src = songURL[current + 1];
+			setCurrent(current + 1);
+
+			setIsPlaying(true);
+			audioRef.current.play();
+		}
+	};
+
+	const previousSong = () => {
+		if (current === 0) {
+			setCurrent(songs.length - 1);
+			audioRef.current.src = songURL[songs.length - 1];
+			setIsPlaying(true);
+			audioRef.current.play();
+		} else {
+			audioRef.current.src = songURL[current - 1];
+			setCurrent(current - 1);
+			setIsPlaying(true);
+			audioRef.current.play();
+		}
+	};
 
 	return (
 		<>
 			<div className="container-fluid">
 				<div className="songs">
-					<ul>{Songs}</ul>
+					<ul>{songs}</ul>
 				</div>
 				<div className="buttonsNav">
-					<button className="btn" type="button" name="previous">
+					<button
+						className="btn"
+						type="button"
+						name="previous"
+						onClick={previousSong}>
 						<i className="fas fa-caret-square-left"></i>
 					</button>
-					<button className="btn" type="button" name="play">
-						<i className="fas fa-play"></i>
+					<button
+						className="btn"
+						type="button"
+						name="play"
+						onClick={togglePlayPause}>
+						{isPlaying ? (
+							<i className="fas fa-pause-circle"></i>
+						) : (
+							<i className="fas fa-play"></i>
+						)}
 					</button>
-					<button className="btn" type="button" name="next">
+					<button
+						className="btn"
+						type="button"
+						name="next"
+						onClick={nextSong}>
 						<i className="fas fa-caret-square-right"></i>
 					</button>
+					<audio ref={audioRef} />
 				</div>
 			</div>
 		</>
